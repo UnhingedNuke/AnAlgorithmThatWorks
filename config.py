3f5f855594d9df3e8beca6422885be4ee78ef638
Unhinged Nuke <unhinged.nuke@gmail.com>
2026-03-28 18:46:24 -0700

    Create config.py
    miss popularity contest

---
+++ config.py
@@ -0,0 +1,16 @@
+#  Title:   config.py
+#  Desc:    Lil bit of smexy matrixy stuff going on around here...
+#  Author:  Angela Louise Trainor
+#  Date:    03/28/2026
+
+WEIGHTS = {                # Core ranking weights
+    "like": 0.25,
+    "reply": 0.35,
+    "repost": 0.25,
+    "dwell": 0.15
+}
+
+BOOSTS = {                  # Boost values
+    "close_friend": 0.3,
+    "recent_post": 0.2
+}
